81481bc491c15641a385410c3684de5af7a56088
ekram-web <unuse2001@gmail.com>
2025-07-15 22:47:58 -0700

    add

--- config/cors.php
+++ config/cors.php
@@ -4,15 +4,13 @@ return [
     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
+        '*', // Allow all origins. For more security, specify your frontend URL(s) here.
         // 'http://localhost:5173',
         // 'http://localhost:5174',
-
-        'https://basirahtv.com',
-    'https://www.basirahtv.com',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-]; 
+];
