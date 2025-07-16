d350d70cf5f5a64da9d7256a0deff4e84a0e58ac
ekram-web <unuse2001@gmail.com>
2025-07-16 01:29:43 -0700

    added

--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -21,4 +21,17 @@
         </div>
         <x-admin.table :items="$contacts" :fields="['address', 'phone', 'email', 'website']" editRoute="admin.contact.edit" deleteRoute="admin.contact.destroy" />
     </div>
-@endsection 
+
+    <!-- Embedded Google Map -->
+    <div class="px-6 pb-10">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.743749299887!2d38.763611!3d9.030833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b853c6d7e2e7b%3A0x7e1e2b6e7e2e7b!2sAddis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2set!4v1710000000000!5m2!1sen!2set"
+            width="100%"
+            height="350"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+    </div>
+@endsection
